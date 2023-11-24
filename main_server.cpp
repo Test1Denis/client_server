883fc6f5f22e75058db96ba7ef598c409aa732df
Denis <ddd@ddd.com>
2023-11-24 16:23:52 +0300

    server with thread... blocking recv

--- main_server.cpp
+++ main_server.cpp
@@ -5,10 +5,44 @@
 #include <sys/types.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <cstring>
+
+
+void* th1(void* arg) {
+	
+		char buf[64];
+		int id_client = *static_cast<int*>(arg);
+		while(1) 
+		{
+			int count_bytes = recv(id_client, buf, 64, 0);
+			if (count_bytes > 0) {
+				std::cout << std::hex << id_client << " : " << count_bytes << " - " << buf << std::endl;
+				memset(buf, 0x00, 64);
+				send(id_client, "OK", 2, 0);
+			}
+			else if (count_bytes < 0) {
+				std::cout << " error " << std::endl;
+				break;
+			}
+			std::cout << "new messages were accepted" << std::endl;
+		}
+
+		std::cout << "-------------------client " << id_client << " error  " << std::endl;
+		close(id_client);
+
+		std::cout << "end thread------------------------------------" << std::endl;
+	
+
+	return 0;
+}
 
 
 int main() {
-	std::cout << "my stupid server... v1.0 " << std::endl;
+	std::cout << "my not full server... v3.0 not to much secret " << std::endl;
+
+
+	pthread_t id_thread;
+
 	int id_socket = socket(AF_INET, SOCK_STREAM, 0);
 	assert(id_socket > 0);
 
@@ -20,24 +54,14 @@ int main() {
 	int res = bind(id_socket, (sockaddr*)&addr, sizeof(addr));
 	assert(res == 0);
 
-	res = listen(id_socket, 5);
+	res = listen(id_socket, 32);
 
 	while(1) {
-
 		int id_client = accept(id_socket, nullptr, nullptr);
+		pthread_create(&id_thread, nullptr, th1, &id_client);
 		assert(id_client > 0);
-
-		char buf[64];
-		//while(1) 
-		{
-			int count_bytes = recv(id_client, buf, 64, 0);
-			if (count_bytes > 0) {
-				std::cout << buf << std::endl;
-				send(id_client, "OK тест 你好", 32, 0);
-			}
-		}
-		close(id_client);
 	}
+
 	close(id_socket);
 
 	return 0;
